0b42c76fdd687638ce10cd761774ea31ed134bc6
shachash1984 <shachash1984@gmail.com>
2019-11-10 08:20:42 +0200

    initial commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public bool isInPosition = true;
+    public Vector3 wantedRot;
+
+    IEnumerator ChangeRotation(Vector3 wantedRot)
+    {
+        Debug.Log("ChangeRotation");
+        while (!isInPosition)
+        {
+            Debug.Log("inLoop");
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(wantedRot), 0.2f);
+            if (transform.rotation.eulerAngles.y - wantedRot.y % 360 < 0.01f)
+                isInPosition = true;
+            yield return new WaitForEndOfFrame();
+        }
+        
+    }
+	void Update () {
+        if (isInPosition)
+        {
+             wantedRot = transform.rotation.eulerAngles;
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                isInPosition = false;
+                wantedRot.y += 90;
+                StartCoroutine(ChangeRotation(wantedRot));
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                isInPosition = false;
+                wantedRot.y -= 90;
+                StartCoroutine(ChangeRotation(wantedRot));
+            }
+        }
+	}
+}
